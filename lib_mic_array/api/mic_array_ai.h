eda692c5e6c96ce1a110cb4b503b71c918d1a4cc
Ed <ed@xmos.com>
2020-08-21 17:41:22 +0100

    Eliminate wrapper when calling from C

--- lib_mic_array/api/mic_array_ai.h
+++ lib_mic_array/api/mic_array_ai.h
@@ -6,12 +6,12 @@
 #include "mic_array_frame.h"
 
 #ifdef __XC__
-void mic_dual_pdm_rx_decimate_ai(
+void mic_dual_pdm_rx_decimate(
         buffered in port:32 p_pdm_mics,
         streaming chanend c_2x_pdm_mic,
         streaming chanend c_ref_audio[]);
 #else
-void mic_dual_pdm_rx_decimate_ai(
+void mic_dual_pdm_rx_decimate(
         port_t p_pdm_mics,
         chanend_t c_2x_pdm_mic,
         chanend_t c_ref_audio[]);
