3750140e667dbd8495b7d91f8fc3dc9ebedd6e90
Ed <ed@xmos.com>
2020-09-04 09:51:53 +0100

    Revert "Remove lib_dsp as dependancy"
    This reverts commit 39e3dd7c79c9ad7893575eb07a730d1a50925b8e.

--- lib_mic_array/api/mic_array_frame.h
+++ lib_mic_array/api/mic_array_frame.h
@@ -4,16 +4,7 @@
 
 #include <stdint.h>
 #include "mic_array_conf.h"
-// #include "dsp_fft.h"
-
-/** Type that represents a complex number. Both the real and imaginary
- * parts are represented as 32-bit fixed point values, with a Q value that
- * is dependent on the use case
- */
-typedef struct {
-    int32_t re;
-    int32_t im;
-} dsp_complex_t;
+#include "dsp_fft.h"
 
 #ifndef MIC_DUAL_ENABLED
     #define MIC_DUAL_ENABLED (0)
